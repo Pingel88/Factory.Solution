e4d16b7fea744d62d8f483be9143fd6e3d71e5f3
Michael Pingel <mdpingel@gmail.com>
2021-03-21 14:01:34 -0700

    add an overview of engineers and the machines they're certified to repair to the home index

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using Factory.Models;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+
     public ActionResult Index()
     {
-      return View();
+      List<Engineer> model = _db.Engineers.ToList();
+      return View(model);
     }
   }
 }
